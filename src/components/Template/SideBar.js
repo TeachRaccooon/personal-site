b82af625abe7f7cf0f305aeabf8ce45d2fb04501
TeachRaccooon <m.y.melnich@gmail.com>
2023-11-16 21:16:24 -0500

    Update

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me2.jpg`} alt="" />
       </Link>
       <header>
         <h2>Maksim Y Melnichenko</h2>
