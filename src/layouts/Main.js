dd88fa398089c110b3f96c60490200ad716ca882
TeachRaccooon <m.y.melnich@gmail.com>
2023-11-16 21:22:11 -0500

    Update

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo" defer={false}>
+    <Helmet titleTemplate="%s | Maksim Melnichenko" defaultTitle="Maksim Melnichenko" defer={false}>
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Michael D'Angelo's personal website.",
+  description: "Personal website of Maksim Melnichenko",
 };
 
 export default Main;
